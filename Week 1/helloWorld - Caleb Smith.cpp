9218e4b042f6a79166b3e59dde90c7d6dc121a80
thatsabummer <145241502+thatsabummer@users.noreply.github.com>
2024-03-05 22:41:51 -0500

    Add files via upload

---
+++ Week 1/helloWorld - Caleb Smith.cpp
@@ -0,0 +1,14 @@
+// Author: Caleb Smith
+// Date: 03.06.2024
+
+// A simple program that prints 'Hello, World!'
+// to be compared to a similar program in Python
+
+#include <iostream> // necessary to print
+
+// The main function that contains our program
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // cout to print, then the text, then endl for a new line
+    return 0;  // Tells the program that no error occurred
+}
