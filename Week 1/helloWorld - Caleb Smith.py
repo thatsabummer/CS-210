5d92a8c3b0b62388103794af9db329ef6ee0bd95
thatsabummer <145241502+thatsabummer@users.noreply.github.com>
2024-03-05 22:51:50 -0500

    Delete Week 1/helloWorld - Caleb Smith.py

--- Week 1/helloWorld - Caleb Smith.py
+++
@@ -1,7 +0,0 @@
-# Author: Caleb Smith
-# Date: 03.06.2024
-
-# A simple program that prints 'Hello, World!'
-# to be compared to a similar program in C++
-
-print("Hello, World!\n")  # a single print statement is all we need
