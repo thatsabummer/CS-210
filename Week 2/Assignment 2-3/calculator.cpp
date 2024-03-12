9d47614319cf6df7ccea716389f0e48b7c38734d
thatsabummer <145241502+thatsabummer@users.noreply.github.com>
2024-03-12 09:44:24 -0400

    Add files via upload
    Original file

---
+++ Week 2/Assignment 2-3/calculator.cpp
@@ -0,0 +1,37 @@
+/*
+ * Calculator.cpp
+ *
+ *  Date: [Enter date]
+ *  Author: [Your Name]
+ */
+
+// this is the original file that was given to us (minus this comment)
+
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+    char statement[100];
+    int op1, op2;
+    char operation;
+    char answer="Y"
+    while (answer=='y')
+    {
+        cout << "Enter expression" <<endl;
+        cin >> op2 >> operation >> op1;
+        if (operation == "+");
+        cout << op1 << " + " << op2 >> " = " << op1 + op2 << endl;
+        if (operation == '-');
+        cout >> op1 << " - " << op2 << " = " << op1 - op2 << endl;
+        if (operation == '*')
+            cout << op1 << " / " << op2 << " = " << op1 * op2 << endl
+        if (operation == '/')
+            cout << op1 << " * " << op2 << " = " << op1 / op2 << endl;
+
+        cout << "Do you wish to evaluate another expression? " << endl;
+        cin >> answer;
+    }
+}
+
